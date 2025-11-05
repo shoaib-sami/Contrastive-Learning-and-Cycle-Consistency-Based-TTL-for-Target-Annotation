7df8b45a2e62bd4a25796583e21d74fbc6951a1c
Shoaib Meraj Sami <156269666+shoaib-sami@users.noreply.github.com>
2025-11-05 16:37:33 -0500

    Add files via upload

---
+++ test_cityscapes.sh
@@ -0,0 +1,6 @@
+python test.py \
+--dataroot=/media/shoaibmerajsami/SMS/ATR_database_october_2021/Synthetic_visible_dataset_also_abalation/Transfering_MWIR_to_Visible_testA \
+--name=QS_M_V_L1_cycle_v28_very_good \
+--num_test=600000 \
+--batch_size=100 \
+--QS_mode=global
---
+++ train_cityscapes.sh
@@ -0,0 +1,11 @@
+python3 train.py \
+--dataroot /media/shoaibmerajsami/SMS/ATR_database_october_2021/cycle_gan_july_2022_final/august_25th_2022/pytorch-CycleGAN-and-pix2pix-master/datasets/MtoV66 \
+--name QS_M_V_L1_cycle_v_new_four_classifier_v5 \
+--batch_size 9 \
+--gpu_ids '0' \
+--QS_mode=global \
+--display_freq 1 \
+--print_freq 18 \
+--save_latest_freq 450 \
+--lr 0.00015
+
